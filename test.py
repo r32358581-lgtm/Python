1ac6d102acc08fb0f77f5f5c4db5267162ce59a7
r32358581-lgtm <r32358581@gmail.com>
2026-04-03 11:37:36 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+def emp()
+    pass
+
+def msg():
+    print("Helllo world!")
+    print("Welcome to python programming")
+    
+    
+def msg2(rep):
+    for i in range(rep):
+        print("This is a repeated message")
+
+    
---
+++ test2.py
@@ -0,0 +1,11 @@
+def rectangle_area(w, h)
+    area = w * h
+    return area
+
+def main():
+    width = 4
+    height = 5
+    area = rectangleArea(width, height)
+    print("The area of the rectangle is:", area)
+    
+    main()
